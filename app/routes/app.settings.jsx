cb3dc6d9bdcfff8e2387d64d41938a422166d759
tahsinAmin <muhammadtahsinamin@gmail.com>
2025-01-16 01:24:01 +0600

    Remix Form data handling with Loader and Actions for Shopify app

--- app/routes/app.settings.jsx
+++ app/routes/app.settings.jsx
@@ -11,11 +11,39 @@ import {
   TextField,
   Divider,
   useBreakpoints,
+  Button,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
+import { useState} from "react";
+import { json } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+
+
+export async function loader() {
+  // provides data to the component
+
+  const settings = {
+    name: "My App",
+    description: "This is a description of my app",
+  }
+
+  return json(settings);
+}
+
+
+export async function action({ request }) {
+
+  let settings = await request.formData();
+  settings = Object.fromEntries(settings);
+  // updates persistent data
+  return json(settings);
+}
+
 
 export default function SettingsPage() {
-  const { smUp } = useBreakpoints();
+  const settings = useLoaderData();
+  const [formState, setFormState] = useState(settings);
+
   return (
     <Page>
       <TitleBar title="Settings" />
@@ -28,61 +56,25 @@ export default function SettingsPage() {
           >
             <BlockStack gap="400">
               <Text as="h3" variant="headingMd">
-                InterJambs
+                Settings
               </Text>
               <Text as="p" variant="bodyMd">
-                Interjambs are the rounded protruding bits of your puzzlie piece
+                Update app settigns and preferences
               </Text>
             </BlockStack>
           </Box>
           <Card roundedAbove="sm">
+            <Form method="POST">
             <BlockStack gap="400">
-              <TextField label="Interjamb style" />
-              <TextField label="Interjamb ratio" />
-            </BlockStack>
+                <TextField label="App Name" name="name" value={formState.name} onChange={(value) => setFormState({ ...formState, name: value })}/>
+                <TextField label="Description" name="description"  value={formState.description} onChange={(value) => setFormState({ ...formState, description: value })}/>
+                  <Button submit={true}>Save</Button>
+              </BlockStack>
+            </Form>
           </Card>
         </InlineGrid>
-        {smUp ? <Divider /> : null}
-        <InlineGrid columns={{ xs: "1fr", md: "2fr 5fr" }} gap="400">
-          <Box
-            as="section"
-            paddingInlineStart={{ xs: 400, sm: 0 }}
-            paddingInlineEnd={{ xs: 400, sm: 0 }}
-          >
-            <BlockStack gap="400">
-              <Text as="h3" variant="headingMd">
-                Dimensions
-              </Text>
-              <Text as="p" variant="bodyMd">
-                Interjambs are the rounded protruding bits of your puzzlie piece
-              </Text>
-            </BlockStack>
-          </Box>
-          <Card roundedAbove="sm">
-            <BlockStack gap="400">
-              <TextField label="Horizontal" />
-              <TextField label="Interjamb ratio" />
-            </BlockStack>
-          </Card>
-        </InlineGrid>
+        
       </BlockStack>
     </Page>
-  );
-}
-
-function Code({ children }) {
-  return (
-    <Box
-      as="span"
-      padding="025"
-      paddingInlineStart="100"
-      paddingInlineEnd="100"
-      background="bg-surface-active"
-      borderWidth="025"
-      borderColor="border"
-      borderRadius="100"
-    >
-      <code>{children}</code>
-    </Box>
   );
 }
